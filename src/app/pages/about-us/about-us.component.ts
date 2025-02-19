2b225144020403384e793da01c905a3bd85ce571
unknown <rgcreation598@gmail.com>
2025-02-19 14:28:18 +0530

    more changes to contact-us page

--- src/app/pages/about-us/about-us.component.ts
+++ src/app/pages/about-us/about-us.component.ts
@@ -11,47 +11,47 @@ import {
 })
 export class AboutUsComponent implements OnInit {
 	iframeSrc: SafeResourceUrl // Use SafeResourceUrl for sanitized iframe URLs
+	searchCity: string = "" // Bind to the search input field
 
-	constructor(private sanitizer: DomSanitizer) {
-		// Default iframe source for Ludhiana
+	// City mapping
+	cityMap = {
+		LUDHIANA:
+			"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d872590.5328929112!2d74.36157277812501!3d31.31599340000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a83ceecb71813%3A0x4d88a1265ec53df8!2sSingla%20Slimming%20Clinic%3A%20Weight%20Loss%20Clinic!5e0!3m2!1sen!2sca!4v1739867189642!5m2!1sen!2sca",
+		JALANDHAR:
+			"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3408.5508095679825!2d75.5785082753004!3d31.31615805720361!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a5bfe544e6bcb%3A0x9d0fc0e206360b23!2sMKC%20Mall!5e0!3m2!1sen!2sin!4v1739870638698!5m2!1sen!2sin",
+		ZIRAKPUR:
+			"https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3432.8583882507537!2d76.81409578147195!3d30.63794832481166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzDCsDM4JzE2LjYiTiA3NsKwNDknMDguMyJF!5e0!3m2!1sen!2sin!4v1739867835870!5m2!1sen!2sin",
+		MOHALI:
+			"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109748.5657591985!2d76.4362851736922!3d30.728444147448787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390feffcc02abd41%3A0xed52846db25d8da4!2sPlaza%20117!5e0!3m2!1sen!2sin!4v1739867926515!5m2!1sen!2sin",
+		AMRITSAR:
+			"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3395.9946491958167!2d74.85414877531358!3d31.661366140163043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3919648dc220ec3f%3A0x334711e8a643a67d!2sD-Block%20Rd%2C%20Gumtala%20Sub%20Urban%2C%20Ranjit%20Avenue%2C%20Amritsar%2C%20Punjab!5e0!3m2!1sen!2sin!4v1739954972100!5m2!1sen!2sin",
+	}
+
+	constructor(private sanitizer: DomSanitizer) {}
+
+	ngOnInit(): void {
+		// Set the default city iframe when component initializes
 		this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl(
-			"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3423.7362470247303!2d75.8206015753214!3d30.894038574506798!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a83ceecb71813%3A0x4d88a1265ec53df8!2sSingla%20Slimming%20Clinic!5e0!3m2!1sen!2sin!4v1685096466393!5m2!1sen!2sin"
+			this.cityMap["LUDHIANA"]
 		)
 	}
 
-	ngOnInit(): void {}
-
-	// Method to change the iframe source based on the location clicked
-	changeLocation(location: string): void {
-		let newIframeSrc = ""
-
-		switch (location) {
-			case "LUDHIANA":
-				newIframeSrc =
-					"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d872590.5328929112!2d74.36157277812501!3d31.31599340000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a83ceecb71813%3A0x4d88a1265ec53df8!2sSingla%20Slimming%20Clinic%3A%20Weight%20Loss%20Clinic!5e0!3m2!1sen!2sca!4v1739867189642!5m2!1sen!2sca"
-				break
-			case "JALANDHAR":
-				newIframeSrc =
-					"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3408.5508095679825!2d75.5785082753004!3d31.31615805720361!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391a5bfe544e6bcb%3A0x9d0fc0e206360b23!2sMKC%20Mall!5e0!3m2!1sen!2sin!4v1739870638698!5m2!1sen!2sin"
-				break
-			case "ZIRAKPUR":
-				newIframeSrc =
-					"https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3432.8583882507537!2d76.81409578147195!3d30.63794832481166!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzDCsDM4JzE2LjYiTiA3NsKwNDknMDguMyJF!5e0!3m2!1sen!2sin!4v1739867835870!5m2!1sen!2sin"
-				break
-			case "MOHALI":
-				newIframeSrc =
-					"https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d109748.5657591985!2d76.4362851736922!3d30.728444147448787!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390feffcc02abd41%3A0xed52846db25d8da4!2sPlaza%20117!5e0!3m2!1sen!2sin!4v1739867926515!5m2!1sen!2sin"
-				break
-			case "AMRITSAR":
-				newIframeSrc =
-					"https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3396.0818946887266!2d74.85143567531347!3d31.658979140281442!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMzHCsDM5JzMyLjMiTiA3NMKwNTEnMTQuNCJF!5e0!3m2!1sen!2sin!4v1739867102895!5m2!1sen!2sin"
-				break
-			default:
-				break
+	// Method to update the iframe source based on the city input
+	onSearch(): void {
+		let city = this.searchCity.toUpperCase().trim() // Get the city input, ensure it's uppercase and trimmed
+		if (this.cityMap[city]) {
+			this.iframeSrc = this.sanitizer.bypassSecurityTrustResourceUrl(
+				this.cityMap[city]
+			)
+		} else {
+			// If the city is not found, keep the default or show an alert
+			alert("City not found! Please enter a valid city name.")
 		}
+	}
 
-		// Sanitize and update the iframe source URL
-		this.iframeSrc =
-			this.sanitizer.bypassSecurityTrustResourceUrl(newIframeSrc)
+	// Method to update iframe when a predefined city is selected
+	changeLocation(location: string): void {
+		this.searchCity = location // Update the search field with the selected city
+		this.onSearch() // Trigger the search method
 	}
 }
--- src/app/photo-gallery/photo-gallery.component.ts
+++ src/app/photo-gallery/photo-gallery.component.ts
@@ -1,89 +1,117 @@
-import { Component, OnInit } from '@angular/core';
-import { AngularFirestore } from '@angular/fire/firestore';
-import { DomSanitizer } from '@angular/platform-browser';
-import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { environment } from 'src/environments/environment';
-import { Images } from '../classes/images';
-import { Videos } from '../classes/video';
-import * as util from '../utils'
+import { Component, OnInit } from "@angular/core"
+import { AngularFirestore } from "@angular/fire/firestore"
+import { DomSanitizer } from "@angular/platform-browser"
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap"
+import { environment } from "src/environments/environment"
+import { Images } from "../classes/images"
+import { Videos } from "../classes/video"
+import * as util from "../utils"
 
 @Component({
-  selector: 'app-photo-gallery',
-  templateUrl: './photo-gallery.component.html',
-  styleUrls: ['./photo-gallery.component.css']
+	selector: "app-photo-gallery",
+	templateUrl: "./photo-gallery.component.html",
+	styleUrls: ["./photo-gallery.component.css"],
 })
 export class PhotoGalleryComponent implements OnInit {
+	images: string[] = []
+	imageGroups: string[][] = [] // Stores images in groups of 4
+	currentIndex: number = 0
+	intervalId: any
+	excludedImages = [7, 8, 10, 11, 17, 22, 26] // List of missing images
+	imagesList: Images[] = []
 
-  imagesList: Images[] = [];
+	videosList: Videos[] = []
+	videoUrl: string
 
-  videosList: Videos[] = [];
-  videoUrl: string;
- 
-  constructor(
-    private db: AngularFirestore,
-    private modalService: NgbModal,
-    private sanitizer:DomSanitizer
-  ) { }
+	constructor(
+		private db: AngularFirestore,
+		private modalService: NgbModal,
+		private sanitizer: DomSanitizer
+	) {}
 
-  ngOnInit(): void {
-    this.getImages();
-    this.getVideos();
-  }
+	ngOnInit(): void {
+		this.getImages()
+		this.getVideos()
+		for (let i = 7; i <= 37; i++) {
+			if (!this.excludedImages.includes(i)) {
+				this.images.push(`IMG-20250210-WA00${i}.jpg`)
+			}
+		}
 
-  openViewVideoModal(modal, videoUrl: string) {
-    this.modalService.open(modal, { size: "lg" });
-    this.videoUrl = videoUrl;
-  }
+		for (let i = 0; i < this.images.length; i += 4) {
+			this.imageGroups.push(this.images.slice(i, i + 4))
+		}
 
+		this.startSlider()
+	}
 
-  getImages() {
-    this.db
-    .collection(util.adminCollection)
-    .doc(environment.adminId)
-    .collection(util.imagesCollection, (ref) =>
-        ref.orderBy("createdOn", "desc")
-      )
-      .get().toPromise()
-      .then((response) => {
-        if (response.docs.length != 0) {
-          response.docs.forEach((ele, idx) => {
-            let sliderbj: Images = Object.assign(
-              {},
-              ele.data() as Images
-            );
-            this.imagesList.push(sliderbj);
-            // this.data.sliderLastDocs.next(ele);
-          });
-          console.log(this.imagesList);
-          
-          // this.data.sliderSub.next(this.sliderImagesList);
-        }
-      });
-  }
+	openViewVideoModal(modal, videoUrl: string) {
+		this.modalService.open(modal, { size: "lg" })
+		this.videoUrl = videoUrl
+	}
 
-  getVideos(){
-    this.db
-    .collection(util.adminCollection)
-    .doc(environment.adminId)
-    .collection(util.videosCollection, (ref) =>
-        ref.orderBy("createdOn", "desc")
-      )
-      .get().toPromise()
-      .then((response) => {
-        if (response.docs.length != 0) {
-          response.docs.forEach((ele, idx) => {
-            let videoObj: Videos = Object.assign(
-              {},
-              ele.data() as Videos
-            );
-            this.videosList.push(videoObj);
-          
-          });
-          console.log(this.imagesList);
-          
-          
-        }
-      });
-  }
+	getImages() {
+		this.db
+			.collection(util.adminCollection)
+			.doc(environment.adminId)
+			.collection(util.imagesCollection, (ref) =>
+				ref.orderBy("createdOn", "desc")
+			)
+			.get()
+			.toPromise()
+			.then((response) => {
+				if (response.docs.length != 0) {
+					response.docs.forEach((ele, idx) => {
+						let sliderbj: Images = Object.assign(
+							{},
+							ele.data() as Images
+						)
+						this.imagesList.push(sliderbj)
+						// this.data.sliderLastDocs.next(ele);
+					})
+					console.log(this.imagesList)
 
+					// this.data.sliderSub.next(this.sliderImagesList);
+				}
+			})
+	}
+
+	getVideos() {
+		this.db
+			.collection(util.adminCollection)
+			.doc(environment.adminId)
+			.collection(util.videosCollection, (ref) =>
+				ref.orderBy("createdOn", "desc")
+			)
+			.get()
+			.toPromise()
+			.then((response) => {
+				if (response.docs.length != 0) {
+					response.docs.forEach((ele, idx) => {
+						let videoObj: Videos = Object.assign(
+							{},
+							ele.data() as Videos
+						)
+						this.videosList.push(videoObj)
+					})
+					console.log(this.imagesList)
+				}
+			})
+	}
+	ngOnDestroy(): void {
+		if (this.intervalId) {
+			clearInterval(this.intervalId)
+		}
+	}
+
+	startSlider(): void {
+		this.intervalId = setInterval(() => {
+			this.next()
+		}, 2000)
+	}
+
+	next(): void {
+		this.currentIndex =
+			(this.currentIndex + 1) % this.imageGroups.length
+	}
 }
